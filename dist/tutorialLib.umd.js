78385ff0ec86bc396b61d14467ff9f0ae9264cec
eloy-almazan <eloy.almazan.riballo@gmail.com>
2024-08-16 10:02:08 +0200

    Add files via upload

---
+++ dist/tutorialLib.umd.js
@@ -0,0 +1,1 @@
+!function(e,o){"object"==typeof exports&&"undefined"!=typeof module?o(exports):"function"==typeof define&&define.amd?define(["exports"],o):o((e="undefined"!=typeof globalThis?globalThis:e||self).tutorialLib={})}(this,(function(e){"use strict";e.greet=function(){console.log("hi")},e.saludar=function(){console.log("saludos")}}));
---
+++ libraries/tutorialLib/index.js
@@ -0,0 +1,8 @@
+export function greet() {
+    console.log('hi');
+}
+
+
+export function saludar() {
+    console.log('saludos');
+}
